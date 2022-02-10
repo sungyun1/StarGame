bb5f66f04cdce665e54f5e4f42659dbbf875dcf6
chmoon0314 <anscodla0314@korea.ac.kr>
2022-02-10 17:03:03 +0900

    using image to draw on UI

--- Scripts/UniverseUpgrade/LineImageDrawer.cs
+++ Scripts/UniverseUpgrade/LineImageDrawer.cs
@@ -12,16 +12,21 @@ public class LineImageDrawer : MonoBehaviour {
 
     public void drawLine(Vector2 p1, Vector2 p2) 
     {
+        print(p1.x);
+        print(p1.y);
         Vector2 fixed1 = p1 + coefficient;
         Vector2 fixed2 = p2 + coefficient;
+        Vector2 directionVector = p2 - p1;
         
         GameObject lineImage = pool.chooseTypeOfPool("lineImage").pullObjectFromPoolTo(LineFolder);
 
         img = lineImage.GetComponent<RectTransform>();
 
-        float magnitude = Vector2.Distance(p1, p2);
-
-        print(img.rect.height);
+        float magnitude = Vector2.Distance(p2, p1);
+        float dotProduct = Vector2.Dot(directionVector, new Vector2(1, 0));
+        
+        img.sizeDelta = new Vector2(magnitude, 7f);
+        print(img.anchoredPosition);
         
     }
 }
